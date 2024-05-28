b89b090cac5ab02b165b7f35f90ab5e9d5738297
湛江生蚝大学 <121650822+Zukede@users.noreply.github.com>
2024-05-28 23:15:41 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,382 @@
+<template>
+<!-- 添加了一行注释 -->
+<!-- 添加了第二行注释 -->
+	<div class="nav">
+		<div class="cp"><a href="">产品</a></div>
+		<div class="fa"><a href="">解决方案</a></div>
+		<div class="zy"><a href="">资源与支持</a></div>
+		<div class="sc"><a href="">商城</a></div>
+	</div>
+	<h1 style="margin-left: 5%;padding-top: 1%;">AMD创新技术</h1>
+	<div class="zhuti">
+		<div class="one">
+			<div class="limgdiv"></div>
+			<h1>AMD Software: Adrenalin Edition</h1>
+			<p>艺术美感 精心打造 直观易用</p>
+			<div class="small1">
+				<div class="more">了解更多</div>
+			</div>
+		</div>
+		<div class="two">
+			<div class="rimgdiv"></div>
+			<h1>AMD "Zen 3" Core Architecture</h1>
+			<p>强大核心助力游戏玩家</p>
+			<div class="small1">
+				<div class="more">了解更多</div>
+			</div>
+		</div>
+	</div>
+	<div class="zhuti2">
+		<div class="firstline">
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>AMD 噪音抑制功能</h2>
+				<p>有效降低周围环境中的背景噪音。</p>
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>AMD Smart Access Memory</h2>
+				<p>CPU 能够完全访问 GPU 内存，带来非凡游戏性能。</p>
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>畅享 AMD Radeon 显卡带来的全新播放体验</h2>
+				
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+		</div>
+		<!-- 第二行 -->
+		<div class="firstline2">
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>AMD CDNA 架构</h2>
+				<p>带来突破性的技术，推动高性能计算与人工智能在百亿亿级时代的融合。</p>
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>AMD FreeSync Premium Pro</h2>
+				<p>AMD FreeSync Premium Pro 树立生动视效新标杆，打造不同凡响的色调映射 HDR 游戏体验。</p>
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>AMD SmartShift 技术</h2>
+				<p>性能有智慧，计算更非凡 根据具体操作智能分配功率。</p>
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+		</div>
+		<!-- 第三行 -->
+		<div class="firstline2">
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>AMD FidelityFX Super Resolution</h2>
+				<p>帮助提高部分游戏的帧率</p>
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>半导体简介</h2>
+				<p>现代电子产品的大脑</p>
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>AMD Infinity Hub</h2>
+				<p>面向高性能计算、人工智能和机器学习的易于部署的软件容器和相关指南</p>
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+		</div>
+		<!-- 第四行 -->
+		<div class="firstline2">
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>AMD PRO 安全</h2>
+				<p>强化到硬件核心的安全性。 采用 AMD PRO 处理器保护业务数据和个人信息。</p>
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>Windows® 11 游戏体验</h2>
+				<p>AMD 计算技术助力 Windows® 11 电脑带来非凡游戏体验</p>
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+			<div class="firstlinediv">
+				<div class="imgdiv1"></div>
+				<h2>AMD PRO 可管理性</h2>
+				<p>不要被困住。 跨平台可管理性意味着你可以按自己的方式部署。</p>
+				<div class="sm1">
+					<div class="more">了解更多</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="show">
+		<div class="wenzi">SHOWMORE</div>
+	</div>
+	<!-- 最后的div -->
+	<div class="over">
+		<div class="line1">
+			<div class="img1">
+				<img src="@/assets/img/微信.png" width="30px" height="30px"/>
+			</div>
+			<div class="img1">
+				<img src="@/assets/img/新浪微博.png" width="30px" height="30px"/>
+			</div>
+			<div class="img1">
+				<img src="@/assets/img/B站.png" width="30px" height="30px"/>
+			</div>
+			<div class="img1">
+				<img src="@/assets/img/邮箱.png" width="30px" height="30px">
+			</div>
+		</div>
+		<div class="line2">
+			<div class="overthe">
+				<h1 class="h1yangshi">公司</h1>
+				<p class="pyangshi">关于AMD</p>
+				<p class="pyangshi">管理团队</p>
+				<p class="pyangshi">企业责任</p>
+				<p class="pyangshi">就业机会</p>
+				<p class="pyangshi">联系我们</p>
+			</div>
+			<div class="overthe">
+				<h1 class="h1yangshi">新闻与活动</h1>
+				<p class="pyangshi">新闻中心</p>
+				<p class="pyangshi">活动</p>
+				<p class="pyangshi">博客</p>
+				<p class="pyangshi">媒体库</p>
+			</div>
+			<div class="overthe">
+				<h1 class="h1yangshi">AMD社区</h1>
+				<p class="pyangshi">支持论坛</p>
+				<p class="pyangshi">开发者</p>
+				<p class="pyangshi">Red Team社区</p>
+			</div>
+			<div class="overthe">
+				<h1 class="h1yangshi">合作伙伴</h1>
+				<p class="pyangshi">AMD合作伙伴中心</p>
+				<p class="pyangshi">合作伙伴资源库</p>
+				<p class="pyangshi">授权经营商</p>
+			</div>
+			<div class="overthe">
+				<h1 class="h1yangshi">投资者</h1>
+				<p class="pyangshi">投资者关系</p>
+				<p class="pyangshi">财务信息</p>
+				<p class="pyangshi">董事会</p>
+				<p class="pyangshi">治理文件</p>
+				<p class="pyangshi">SEC报告</p>
+			</div>
+		</div>
+		<div class="final">
+			<p class="pyangshi2">京ICP备12018899号-2| ​条款和条件| 隐私| 商标| 强迫劳动声明| 公开公平竞争| 英国税收策略| Cookie 政策| Cookie 设置</p>
+		</div>
+		<div class="final">
+			<p class="pyangshi2">© 2024 Advanced Micro Devices, Inc.</p>
+		</div>
+	</div>
+	
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.final{
+	width: 800px;
+	height: 50px;
+	margin: 0 auto;
+	background-color: rgb(0, 0, 0);
+	text-align: center;
+}
+.pyangshi{
+	color: rgb(190, 190, 190);
+}
+.pyangshi2{
+	color: rgb(190, 190, 190);
+	font-size: 13px;
+}
+.h1yangshi{
+	color: white;
+	font-size: 20px;
+}
+.overthe{
+	width: 15%;
+	height: 100%;
+	background-color: #000000;
+}
+.line2{
+	background-color: rgb(0, 0, 0);
+	width: 70%;
+	height: 300px;
+	margin: 0 auto;
+	margin-top: 50px;
+	display: flex;
+	justify-content: space-between;
+}
+.img1{
+	width: 30px;
+	height: 30px;
+	background-color: rgb(0, 0, 0);
+	margin-right: 2%;
+}
+.line1{
+	width: 80%;
+	height: 100px;
+	background-color: rgb(0, 0, 0);
+	margin: 0 auto;
+	display: flex;
+	justify-content: flex-end;
+	align-items: flex-end;
+}
+.over{
+	width: 100%;
+	height: 600px;
+	margin-top: 1%;
+	background-color: rgb(0, 0, 0);
+}
+.wenzi{
+	font-size: 20px;
+}
+.show{
+	width: 200px;
+	height: 70px;
+	background-color: rgb(204, 204, 204);
+	margin: 0 auto;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border: 2px solid black;
+}
+.nav{
+	background-color: black;
+	height: 90px;
+	width: 100%;
+	display: flex;
+	align-items: center;
+}
+.cp{
+	margin-left: 5%;
+	color: white;
+}
+.fa{
+	margin-left: 5%;
+	color: white;
+}
+.zy{
+	margin-left: 5%;
+	color: white;
+}
+.sc{
+	margin-left: 5%;
+	color: white;
+}
+.zhuti{
+	background-color: darkgray;
+	width: 100%;
+	height: 600px;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+}
+.one{
+	background-color: dimgray;
+	width: 45%;
+	height: 500px;
+	
+}
+.two{
+	background-color: dimgray;
+	width: 45%;
+	height: 500px;
+	
+}
+.limgdiv{
+	width: 100%;
+	height: 300px;
+	background-color: darkred;
+}
+.rimgdiv{
+	width: 100%;
+	height: 300px;
+	background-color: darkred;
+}
+.small1{
+	background-color: dimgray;
+	border: 2px solid black;
+	width: 180px;
+	height: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.zhuti2{
+	width: 100%;
+	height: 1750px;
+	background-color:Gainsboro;
+	margin-top: 30px;
+}
+a{
+	text-decoration: none;
+	color: white;
+}
+.firstline{
+	width: 100%;
+	height: 400px;
+	background-color: dimgray;
+	display: flex;
+	justify-content: space-evenly;
+}
+.firstline2{
+	width: 100%;
+	height: 400px;
+	background-color: dimgray;
+	display: flex;
+	justify-content: space-evenly;
+	margin-top: 30px;
+}
+.firstlinediv{
+	background-color: darkgray;
+	width: 30%;
+	height: 100%;
+	
+}
+.imgdiv1{
+	background-color: darkred;
+	width: 100%;
+	height: 200px;
+}
+.sm1{
+	background-color: darkgray;
+	border: 2px solid black;
+	width: 180px;
+	height: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
